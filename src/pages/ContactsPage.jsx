11ea386e8f98243a8681a0838a875bbae0412550
Motkova Kateryna <sirenkoekaterina90@gmail.com>
2023-11-04 11:12:45 +0200

    6

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,16 +1,16 @@
 import PhoneBook from 'modules/PhoneBook/PhoneBook'
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { selectAllContacts, selectError, selectIsLoading } from 'redux/contacts/contacts-selectors';
-import { selectFilter } from 'redux/filter/filter-selectors';
+// import { useDispatch, useSelector } from 'react-redux'
+// import { selectAllContacts, selectError, selectIsLoading } from 'redux/contacts/contacts-selectors';
+// import { selectFilter } from 'redux/filter/filter-selectors';
 
 const ContactsPage = () => {
 
-  const dispatch = useDispatch();
-  const contacts = useSelector(selectAllContacts);
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
-  const filter = useSelector(selectFilter);
+  // const dispatch = useDispatch();
+  // const contacts = useSelector(selectAllContacts);
+  // const isLoading = useSelector(selectIsLoading);
+  // const error = useSelector(selectError);
+  // const filter = useSelector(selectFilter);
   return (
     <PhoneBook/>
   )
